5ab92a283b6844b65c969adca9dc4995a04e692c
AnanoBB <ana.berishvili.1@btu.edu.ge>
2020-07-20 19:42:41 +0400

    new heros page added/few changes for form

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,3 +2,26 @@ function play() {
     var audio = document.getElementById("audio");
     audio.play();
   }
+
+  
+
+  function openForm(){
+    let btn = document.getElementById('btn');
+    let click = document.getElementById('overlay');
+    if(click.style.display=="none"){
+      click.style.display = "block";
+    }
+    else{
+      click.style.display = 'none';
+    }
+    if (btn.textContent === "LOGIN") {
+      btn.textContent = "X";
+    } else {
+      btn.textContent = "LOGIN";
+    }
+  }
+
+
+
+  
+  
